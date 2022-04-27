a60b30e7996af8728504b7a1fc6833d2b7ceb07e
Sydney Hoglund <hoglu096@d.umn.edu>
2022-04-27 17:39:18 -0500

    created folder for pages and coffee_time_page.dart

---
+++ lib/pages/coffee_time_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class CoffeeTime extends StatefulWidget {
+  @override
+  _CoffeeTime createState() => _CoffeeTime();
+}
+
+class _CoffeeTime extends State<CoffeeTime>{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text ('View by Coffee Time'),
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  DropdownButton(items: items, onChanged: onChanged),
+                  DropdownButton(items: items, onChanged: onChanged),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
