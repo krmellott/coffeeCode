904c100d371ff9b621484b4717382a2909e7b03c
Spencer Leisch <leisc020@d.umn.edu>
2022-04-28 18:23:42 -0500

    created journal page and add journal page

---
+++ lib/pages/coffee_journal_page.dart
@@ -0,0 +1,149 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:the_coffee_and_code/utils/add_journal_page.dart';
+import 'package:flutter/material.dart';
+import 'package:the_coffee_and_code/controller/BCController.dart';
+
+import '../utils/add_journal_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp(options: defaultFirebaseOptions);
+  runApp(MainJournal());
+}
+
+///important variables
+final journalsRef = FirebaseFirestore.instance.collection('journal');
+final StressFreeController controllerRef = StressFreeController();
+
+class MainJournal extends StatefulWidget {
+  @override
+  _MainJournal createState() => _MainJournal();
+}
+
+class _MainJournal extends State<MainJournal> {
+  String dropDownValue = "default";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Journal'),
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.lightBlueAccent, Colors.white])),
+          child: Column(children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 5.0),
+              child: Text("Your Journals", // the title
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  )),
+            ),
+            Expanded(
+                child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('journal')
+                        .snapshots(),
+                    builder:
+                        (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData) {
+                        return Text("We didn't find any journals for you");
+                      }
+                      final data = snapshot.requireData;
+                      return ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: data.size,
+                          itemBuilder: (context, index) {
+                            return _buildJournalListItem(
+                                context, data.docs[data.size - 1 - index]);
+
+                            ///this is really dumb, but it makes the list auto sort to newest to oldest
+                            ///please don't change this
+                          });
+                    })
+            ),
+            OutlinedButton(
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.fromLTRB(50.0, 1.0, 50.0, 1.0)),
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+              ),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (BuildContext context) {
+                  return AddEntry();
+                }));
+              },
+              //backgroundColor: Colors.green,
+              child: const Text('New Journal Entry', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
+            )
+          ]),
+        ),
+      ),
+
+    );
+  }
+}
+
+///Creates a list of all journal in order of creation from most recent to least
+///Returns a card consisting of the title and the body
+Widget _buildJournalListItem(BuildContext context, DocumentSnapshot document) {
+  var data = document.data() as Map<String, dynamic>;
+  var date = DateTime.fromMillisecondsSinceEpoch(data['date']);
+  return Padding(
+    padding: const EdgeInsets.all(1.0),
+    child: Card(
+      child: ListTile(
+          contentPadding: EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 0.0),
+          title: Text(data['title'] +
+              " --- " +
+              date.month.toString() +
+              "/" +
+              date.day.toString() +
+              "/" +
+              date.year.toString()),
+          subtitle: Text(data['body']),
+          trailing: TextButton(
+            child: Icon(Icons.cancel_sharp, color: Colors.red),
+            onPressed: () {
+              showAlertDialog(context, data['title'].toString());
+            },
+          )),
+    ),
+  );
+}
+
+showAlertDialog(BuildContext context, String title) {
+  Widget yesButton = TextButton(
+    child: Text("YES"),
+    onPressed: () {
+      controllerRef.removeJournalData('journal', title);
+      Navigator.of(context).pop();
+    },
+  );
+  Widget noButton = TextButton(
+    child: Text("NO"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+  AlertDialog alert = AlertDialog(
+    title: Text("Would you like to delete this journal entry?"),
+    actions: [
+      yesButton,
+      noButton,
+    ],
+  );
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
---
+++ lib/utils/add_journal_page.dart
@@ -0,0 +1,111 @@
+
+import 'package:flutter/material.dart';
+
+import '../controller/BCController.dart';
+
+class AddEntry extends StatefulWidget {
+  const AddEntry({Key? key}) : super(key: key);
+
+  @override
+  State<AddEntry> createState() => _AddEntry();
+}
+
+class _AddEntry extends State<AddEntry> {
+  StressFreeController controllerRef = StressFreeController();
+  String body = "";
+  String title = "";
+
+  final ButtonStyle saveButtonStyle = ElevatedButton.styleFrom(
+    // ButtonStyle for the save button
+      primary: Colors.green,
+      textStyle: const TextStyle(fontSize: 20.0),
+      padding: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("New Journal Entry"),
+        ),
+        body: Center(
+            child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.lightBlueAccent, Colors.white])),
+                child: Column(children: [
+                  Row(children: [
+                    Expanded(
+                      child: Container(
+                        margin:
+                        const EdgeInsets.only(left: 20, right: 10, top: 10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Title your entry!',
+                          ),
+                          onChanged: (String? aTitle) {
+                            setState(() {
+                              title = aTitle!;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      child: const Text('Save'),
+                      onPressed: () {
+                        if (body == "" || title == "") {
+                          showAlertDialog(context); //displays ominous threat
+                        } else {
+                          controllerRef.insertJournalData(body,
+                              DateTime.now().millisecondsSinceEpoch, title);
+                          Navigator.pop(
+                              context); //submits the completed journal
+                        }
+                      },
+                      style: saveButtonStyle,
+                    )
+                  ]),
+                  Container(
+                    margin: const EdgeInsets.only(left: 20, right: 10, top: 10),
+                    child: TextField(
+                      maxLines: null,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'What did you do today?',
+                      ),
+                      onChanged: (String? aBody) {
+                        setState(() {
+                          body = aBody!;
+                        });
+                      },
+                    ),
+                  ),
+                ]))));
+  }
+}
+
+/// Creates and manages the alert dialog that tells users to fill in
+/// all fields, thus preventing users from submitting empty journals.
+showAlertDialog(BuildContext context) {
+  Widget okButton = TextButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+  AlertDialog alert = AlertDialog(
+    title: Text("It seems you left a field blank. Please fill it out to save your journal."),
+    actions: [
+      okButton,
+    ],
+  );
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
