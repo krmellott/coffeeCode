c78a597fe47339f954944920a847698d9845c944
Gideon Okoroafor <okoro062@umn.edu>
2022-05-02 19:41:18 -0500

    image button class

---
+++ lib/utils/image_buttons.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ImageButton extends StatelessWidget {
+  final String label;
+  final String image;
+  final Function() onTap;
+  const ImageButton(
+      {Key? key, required this.label, required this.onTap, required this.image})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        child: Column(
+          // alignment: Alignment.center,
+            children: <Widget>[
+              Ink.image(
+                image: NetworkImage(image),
+                height: 250,
+                width: 185,
+                fit: BoxFit.cover,
+                child: InkWell(
+                  onTap: onTap,
+                ),
+              ),
+              Text(
+                label,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              )
+            ]),
+      ),
+    );
+  }
+}
