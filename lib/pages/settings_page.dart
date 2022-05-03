f8cf95bee1c90c584ae2384ff100cc7f54b96597
Jacob Rohde <rohde135@d.umn.edu>
2022-05-03 17:53:04 -0500

    Updated the settings page to now include options to subscribe/unsubscribe from coding or coffee related notifications, and a sign out button that signs you out and boots you to the login page.

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -2,6 +2,8 @@
 /// A page that displays settings for the app.
 /// @author Jacob Rohde
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:the_coffee_and_code/pages/videos_page.dart';
 
@@ -17,58 +19,168 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPage extends State<SettingsPage> {
-  //AppTheme theme = AppTheme();
   bool themeIsOn = false;
-  // Color mainColor = const Color.fromARGB(255, 0, 255, 0);
-  // Color backgroundColor = Colors.black;
-
+  bool coffeeNotifIsOn = false;
+  bool codeNotifIsOn = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Settings', style: TextStyle(color: theme.textColor)),
-          backgroundColor: theme.barColor,
-          iconTheme: IconThemeData(color: theme.textColor),
-        ),
-        body: Center(
-            child: Container(
-                decoration: BoxDecoration(color: theme.backgroundColor),
-                child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                        children: [
+      appBar: AppBar(
+        title: Text('Settings', style: TextStyle(color: theme.textColor)),
+        backgroundColor: theme.barColor,
+        iconTheme: IconThemeData(color: theme.textColor),
+      ),
+      body: Center(
+          child: Container(
+              decoration: BoxDecoration(color: theme.backgroundColor),
+              child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                      children: [
 
-                      Row(
-                        children: [
-                          Text('Light Mode: ',
-                              style: TextStyle(
-                                  color: theme.textColor,
-                                  fontSize: 25.0,
-                                  fontFamily: 'CutiveMono')
+                        Row(
+                            children: [
+                              Text('Light Mode: ',
+                                  style: TextStyle(
+                                      color: theme.textColor,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'CutiveMono')
+                              ),
+
+                              Switch(
+                                onChanged: (isOn) {
+                                  theme.switchTheme();
+
+                                  setState(() {
+                                    themeIsOn = isOn;
+                                  });
+                                },
+                                value: !theme.isDark,
+                                inactiveThumbColor: const Color.fromARGB(255, 0, 255, 0),
+                                inactiveTrackColor: const Color.fromARGB(255, 0, 100, 0),
+
+                              )
+                            ]
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 25, 0, 40),
+                            child: Column (
+                                children: [
+
+                                  Text('Notifications',
+                                      style: TextStyle(
+                                          color: theme.textColor,
+                                          fontSize: 25.0,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'CutiveMono')
+                                  ),
+
+                                  Row(
+                                      children: [
+                                        Text('Coffee Info: ',
+                                            style: TextStyle(
+                                                color: theme.textColor,
+                                                fontSize: 15.0,
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: 'CutiveMono')
+                                        ),
+
+                                        OutlinedButton(
+                                          style: ButtonStyle(
+                                              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.fromLTRB(5.0, 1.0, 5.0, 1.0)),
+                                              backgroundColor: MaterialStateProperty.all<Color>(theme.backgroundColor),
+                                              side: MaterialStateProperty.all(BorderSide(color: theme.mainColor, width: 2))
+                                          ),
+                                          onPressed: () async {
+                                            print('coffee notifications on');
+                                            await FirebaseMessaging.instance.subscribeToTopic("coffee");
+                                          },
+                                          //backgroundColor: Colors.green,
+                                          child: Text('Subscribe', style: TextStyle(fontWeight: FontWeight.bold, color: theme.textColor, fontFamily: 'CutiveMono')),
+                                        ),
+
+                                        OutlinedButton(
+                                          style: ButtonStyle(
+                                              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.fromLTRB(5.0, 1.0, 5.0, 1.0)),
+                                              backgroundColor: MaterialStateProperty.all<Color>(theme.backgroundColor),
+                                              side: MaterialStateProperty.all(BorderSide(color: theme.mainColor, width: 2))
+                                          ),
+                                          onPressed: () async {
+                                            print('coffee notifications off');
+                                            await FirebaseMessaging.instance.unsubscribeFromTopic("coffee");
+                                          },
+                                          //backgroundColor: Colors.green,
+                                          child: Text('Unsubscribe', style: TextStyle(fontWeight: FontWeight.bold, color: theme.textColor, fontFamily: 'CutiveMono')),
+                                        )
+                                      ]
+                                  ),
+
+                                  Row(
+                                      children: [
+                                        Text('Coding Info: ',
+                                            style: TextStyle(
+                                                color: theme.textColor,
+                                                fontSize: 15.0,
+                                                fontWeight: FontWeight.bold,
+                                                fontFamily: 'CutiveMono')
+                                        ),
+
+                                        OutlinedButton(
+                                          style: ButtonStyle(
+                                              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.fromLTRB(5.0, 1.0, 5.0, 1.0)),
+                                              backgroundColor: MaterialStateProperty.all<Color>(theme.backgroundColor),
+                                              side: MaterialStateProperty.all(BorderSide(color: theme.mainColor, width: 2))
+                                          ),
+                                          onPressed: () async {
+                                            print('code notifications on');
+                                            await FirebaseMessaging.instance.subscribeToTopic("code");
+                                          },
+                                          //backgroundColor: Colors.green,
+                                          child: Text('Subscribe', style: TextStyle(fontWeight: FontWeight.bold, color: theme.textColor, fontFamily: 'CutiveMono')),
+                                        ),
+
+                                        OutlinedButton(
+                                          style: ButtonStyle(
+                                              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.fromLTRB(5.0, 1.0, 5.0, 1.0)),
+                                              backgroundColor: MaterialStateProperty.all<Color>(theme.backgroundColor),
+                                              side: MaterialStateProperty.all(BorderSide(color: theme.mainColor, width: 2))
+                                          ),
+                                          onPressed: () async {
+                                            print('code notifications off');
+                                            await FirebaseMessaging.instance.unsubscribeFromTopic("code");
+                                          },
+                                          //backgroundColor: Colors.green,
+                                          child: Text('Unsubscribe', style: TextStyle(fontWeight: FontWeight.bold, color: theme.textColor, fontFamily: 'CutiveMono')),
+                                        )
+
+
+                                      ]
+                                  ),
+                                ]
+                            )
+                        ),
+
+                        OutlinedButton(
+                          style: ButtonStyle(
+                              padding: MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.fromLTRB(50.0, 1.0, 50.0, 1.0)),
+                              backgroundColor: MaterialStateProperty.all<Color>(theme.backgroundColor),
+                              side: MaterialStateProperty.all(BorderSide(color: theme.mainColor, width: 2))
                           ),
+                          onPressed: () {
+                            FirebaseAuth.instance.signOut();
+                            Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
+                          },
+                          //backgroundColor: Colors.green,
+                          child: Text('Sign Out', style: TextStyle(fontWeight: FontWeight.bold, color: theme.textColor, fontFamily: 'CutiveMono')),
+                        )
 
-                          Switch(
-                            onChanged: (isOn) {
-                              theme.switchTheme();
-
-                              setState(() {
-                                themeIsOn = isOn;
-                              });
-                            },
-                            value: !theme.isDark,
-                            inactiveThumbColor: const Color.fromARGB(255, 0, 255, 0),
-                            inactiveTrackColor: const Color.fromARGB(255, 0, 100, 0),
-
-                          )
-                        ]
-                      ),
-                      Row(),
-                    ]
-                    )
-                )
-            )
-        ),
+                      ]
+                  )
+              )
+          )
+      ),
       drawer: Drawer(
           backgroundColor: theme.backgroundColor,
           child: ListView(
