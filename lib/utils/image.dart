78d4b953a3cd094e2ec63004a0a083f41f4a36ba
Gideon Okoroafor <okoro062@umn.edu>
2022-05-02 19:41:05 -0500

    image class

--- lib/utils/image.dart
+++ lib/utils/image.dart
@@ -1,7 +1,7 @@
 class Images {
-  static get background_image => '';
-  static get brewing_coffee_image => '';
-  static get coding_tutorial_image => '';
-  static get coffee_recipes_image => '';
-
+  static String get brewing_coffee_image => 'https://cdn.pixabay.com/photo/2020/09/24/11/30/coffee-5598374_960_720.png';
+  static String get coding_tutorial_image => 'https://wallpaperaccess.com/full/314821.jpg';
+  static String get coffee_recipes_image => 'https://thumbs.dreamstime.com/b/coffee-book-recipe-logo-design-vector-icon-shop-library-90654852.jpg';
+  static String get favorites => 'https://i.pinimg.com/564x/31/64/c8/3164c88a698819042dde3803aa6d45b8.jpg';
+  static String get lofi_image => 'https://i.ytimg.com/vi/xZ2GidJCpxE/maxresdefault.jpg';
 }
